712486d50cfd6fe6ce344a28c02e68d81fa7db74
jzide <jzide@JZSB>
2021-01-25 11:33:56 +0900

    update

--- vramig/ttt.py
+++ vramig/ttt.py
@@ -15,18 +15,11 @@ from module.common import VRASession, jpp, setDebugMode
 setDebugMode()
 vra = VRASession('vra.vmkloud.com', 'jangh', 'David*#8090')
 
-target = 'test1'
-
-resources = vra.get("/provisioning/uerp/resources/compute?expand&$filter=((type eq 'VM_GUEST') and (name eq '%s') and (lifecycleState eq 'READY'))" % target)
-jpp(resources['documentLinks'])
-resourceLink = resources['documentLinks'][0]
-resource = resources['documents'][resourceLink]
-resourceName = resource['name']
-jpp(resource)
-
-
-name = 'ob-%s' % target
-description = 'onboard test %s' % target
+ns = 'test'
+vms = ['test2', 'test3']
+bp = '[DEMO] Dynamic VM'
+name = 'ob-%s' % ns
+description = 'onboard test %s' % ns
 
 # Cloud Account
 endpointIds = ['8967fe8c-e6e6-46b9-a3b5-d4ab9d4a053e']
@@ -35,9 +28,20 @@ endpointIds = ['8967fe8c-e6e6-46b9-a3b5-d4ab9d4a053e']
 
 # Project
 projectId = '85a2bc48-f42f-4e38-b160-994f8a5f2127'
-templateLink = '/blueprint/api/blueprints/b78e547c-1efc-44bd-b0f9-0f5e2a40afa5'
-# /blueprint/api/blueprints
-# d['selfLink']
+
+# Get Resources
+resources = []
+for vm in vms:
+    rs = vra.get("/provisioning/uerp/resources/compute?expand&$filter=((type eq 'VM_GUEST') and (name eq '%s') and (lifecycleState eq 'READY'))" % vm)
+    rLink = rs['documentLinks'][0]
+    r = rs['documents'][rLink]
+    rName = r['name']
+    resources.append((rLink, rName))
+
+# Get Blueprint
+blueprints = vra.get("/blueprint/api/blueprints?&search=Dynamic")
+blueprint = blueprints['content'][0]
+blueprintLink = blueprint['selfLink']
 
 #############################################################
 # Create Plan
@@ -61,21 +65,22 @@ dep = vra.post('/relocation/onboarding/deployment', dep_payload)
 depLink = dep['documentSelfLink']
  
 # Create Resource
-rsc_payload = {
-    'planLink': planLink,
-    'deploymentLink': depLink,
-    'resourceLink': resourceLink,
-    'resourceName': resourceName,
-    'ruleLinks': []
-}
-rsc = vra.post('/relocation/onboarding/resource', rsc_payload)
+for resource in resources:
+    rsc_payload = {
+        'planLink': planLink,
+        'deploymentLink': depLink,
+        'resourceLink': resource[0],
+        'resourceName': resource[1],
+        'ruleLinks': []
+    }
+    rsc = vra.post('/relocation/onboarding/resource', rsc_payload)
  
 # Create Blueprint
 bp_payload = {
     'name': name,
     'planLink': planLink,
     'deploymentLink': depLink,
-    'templateLink': templateLink,
+    'templateLink': blueprintLink,
     'autoGenerate': False
 }
 bp = vra.post('/relocation/onboarding/blueprint', bp_payload)
